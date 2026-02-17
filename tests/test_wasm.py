82494d2c2dd30d62e4c290c5f9539d20da969b54
Alexis Jeandet <alexis.jeandet@member.fsf.org>
2026-02-17 17:28:23 +0100

    Add WASM CI

---
+++ tests/test_wasm.py
@@ -0,0 +1,22 @@
+try:
+    from pytest_pyodide import run_in_pyodide
+    from pytest_pyodide.decorator import copy_files_to_pyodide
+    import pytest
+
+    from glob import glob
+
+    _FILE_PATH = glob("pyodide-dist/astralint*.whl", recursive=True)[0]
+    _DEST_PATH = _FILE_PATH.split("/")[-1]
+
+
+    @pytest.mark.driver_timeout(60 * 2)
+    @copy_files_to_pyodide(file_list=[(_FILE_PATH, _DEST_PATH)], install_wheels=True, recurse_directories=True)
+    @run_in_pyodide(packages=['micropip', 'pycdfpp'])
+    async def test_import_astralint(selenium):
+        import astralint as ai
+        from astralint.base import list_all_suites
+        assert len(list_all_suites()) > 0
+
+
+except ImportError:
+    print("pytest-pyodide is not installed; skipping Pyodide tests.")
